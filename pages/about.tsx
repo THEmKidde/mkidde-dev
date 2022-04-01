232d7b38fc8eee5bfda814e959ea9447c633072f
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-01 17:54:13 +0200

    Added last about me information

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Container from '../components/Container'
+import Tag from '../components/Tag'
 import Image from 'next/image'
+import { FaDatabase } from 'react-icons/fa'
+import { SiCsharp, SiJava, SiJavascript, SiPython, SiTypescript, SiC, SiNextdotjs, SiReact, SiDocker, SiGit, SiNodedotjs, SiUnity, SiMongodb } from 'react-icons/si'
 
 export default function About() {
     return (
@@ -34,9 +37,31 @@ export default function About() {
                                 learning how to do so 'properly'. 
                             </h2>
                             <p className="text-base md:text-lg mt-8">
-                                I love exploring and learning new technologies, furthering my knowledge and improving my skills in the field. 
-                                I mainly focus on backend, but do also enjoy the creative possibilities of frontend developing.
-                                
+                                I mainly focus on backend, but do also enjoy the creative possibilities of frontend developing. <br />
+                                For the most part I work with Object Oriented Programming languages such as C# or Java, but I do also have experience
+                                in less OOP based languages such as Python and JavaScript, as well as some useful tools and frameworks listed below.   <br />
+                                <div className="mt-4 flex flex-wrap justify-center space-x-3 space-y-1">
+                                    <Tag title="C#" className="mt-1"><SiCsharp className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Java"><SiJava className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Python"><SiPython className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="C"><SiC className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="JavaScript"><SiJavascript className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="TypeScript"><SiTypescript className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="SQL"><FaDatabase className="text-lg text-neutral-800"/></Tag>
+                                </div>
+                                <div className="mt-3 mb-8 flex flex-wrap justify-center space-x-3 space-y-1">
+                                    <Tag title="Git" className="mt-1"><SiGit className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Unity"><SiUnity className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Node.js"><SiNodedotjs className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="React"><SiReact className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Next.js"><SiNextdotjs className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="MongoDB"><SiMongodb className="text-lg text-neutral-800"/></Tag>
+                                    <Tag title="Docker"><SiDocker className="text-lg text-neutral-800"/></Tag>
+                                </div>
+                                My programming journey began by making video games and attempting to recreate well-known games, such as the old Pong and Tetris games.
+                                From there my interest for programming grew, and I wanted to know more and more about the possibilities and what else you would be 
+                                able to create with it. I have always been very creative, and have always loved creating things, whether it being a Lego spaceship 
+                                or a wooden bench. I love exploring and learning new technologies, furthering my knowledge and improving my skills in the field. <br /> <br />
                             </p>
                         </article>
                         <div className="w-2/5 mx-6 mt-8 hidden md:block">
