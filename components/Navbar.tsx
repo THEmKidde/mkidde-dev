c942690c2d0f7fac6528b11e8e371f0e2fc81fa5
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-01 17:28:04 +0200

    added burger menu for mobile use

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,32 @@
-import Container from "./Container"
+import Container from './Container'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { useState } from 'react'
 
 export default function Navbar({ page }) {
+    const [menuToggle, setMenuToggle] = useState(false)
     return (
         <>
             <div className="fixed top-0 w-full z-20 bg-white shadow-lg shadow-gray-100">
                 <Container className="flex justify-between h-16">
                     <h1 className="self-center text-xl font-semibold text-neutral-800 select-none">mkt.</h1>
-                    <div className="self-center">
-                        {page == "home" ? 
-                            <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 cursor-pointer">Home</a>
-                            : <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100" href="/">Home</a> 
-                        } 
-                        {page == "about" ? 
-                            <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 cursor-pointer">About</a>
-                            : <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100" href="/about">About</a>
-                        }
-                        {page == "projects" ?
-                            <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 cursor-pointer">Projects</a>
-                            : <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100" href="/projects">Projects</a>
-                        }
-                        {page == "contact" ? 
-                            <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 cursor-pointer">Contact</a>
-                            : <a className="mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100" href="/contact">Contact</a>
-                        }
+                    <div className="block sm:hidden">
+                        <AiOutlineMenu className={"text-3xl select-none mr-2 " + (menuToggle ? "hidden" : "block")} onClick={()=>{setMenuToggle(true)}}/>
+                        <AiOutlineClose className={"text-3xl select-none mr-2 " + (menuToggle ? "block" : "hidden")} onClick={()=>{setMenuToggle(false)}}/>
+                    </div>
+                    <div className="self-center hidden sm:flex decoration-2 decoration-cyan-500 underline-offset-2">
+                        <a href="/" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "home" ? "pointer-events-none underline":"")}>Home</a> 
+                        <a href="/about" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "about" ? "pointer-events-none underline":"")}>About</a>
+                        <a href="/projects" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
+                        <a href="/contact" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
                     </div>
                 </Container>
+                <div className={"fixed top-0 left-0 w-full bg-neutral-700 bg-opacity-30 mt-16 flex flex-col justify-center items-center overflow-scroll md:hidden decoration-2 decoration-cyan-500 underline-offset-2 " + (menuToggle ? "block" : "hidden")}>
+                    <a href="/" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "home" ? "pointer-events-none underline":"")}>Home</a>
+                    <a href="/about" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "about" ? "pointer-events-none cunderline":"")}>About</a>
+                    <a href="/projects" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
+                    <a href="/contact" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
+                    <div className="h-screen" />
+                </div>
             </div> 
         </>
     )
