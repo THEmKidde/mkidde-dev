4d33524720bc1368e41cc9b87b8ed7f916529cd1
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-02 17:44:52 +0200

    updated img domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-      domains: ['media-exp1.licdn.com', 'www.adobe.com'],
+      domains: ['media-exp1.licdn.com', 'www.adobe.com', 'i.ytimg.com'],
     },
     env: {
         FORM_URL: process.env.FORM_URL,
