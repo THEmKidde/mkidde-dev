3705f4f0ee08f111a3dc27a024abbb166b9d884f
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-02 16:48:34 +0200

    Now uses Next.js Link tag instead of regular anchors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Container from './Container'
+import Link from 'next/link'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 import { useState } from 'react'
 
@@ -14,17 +15,33 @@ export default function Navbar({ page }) {
                         <AiOutlineClose className={"text-3xl select-none mr-2 " + (menuToggle ? "block" : "hidden")} onClick={()=>{setMenuToggle(false)}}/>
                     </div>
                     <div className="self-center hidden sm:flex decoration-2 decoration-cyan-500 underline-offset-2">
-                        <a href="/" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "home" ? "pointer-events-none underline":"")}>Home</a> 
-                        <a href="/about" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "about" ? "pointer-events-none underline":"")}>About</a>
-                        <a href="/projects" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
-                        <a href="/contact" className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
+                        <Link href="/">
+                            <a className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "home" ? "pointer-events-none underline":"")}>Home</a> 
+                        </Link>
+                        <Link href="/about">
+                            <a className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "about" ? "pointer-events-none underline":"")}>About</a>
+                        </Link>
+                        <Link href="/projects">
+                            <a className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
+                        </Link>
+                        <Link href="/contact">
+                            <a className={"mx-3 text-lg font-medium text-neutral-800 select-none hover:text-cyan-500 duration-100 " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
+                        </Link>
                     </div>
                 </Container>
                 <div className={"fixed top-0 left-0 w-full bg-neutral-700 bg-opacity-30 mt-16 flex flex-col justify-center items-center overflow-scroll md:hidden decoration-2 decoration-cyan-500 underline-offset-2 " + (menuToggle ? "block" : "hidden")}>
-                    <a href="/" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "home" ? "pointer-events-none underline":"")}>Home</a>
-                    <a href="/about" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "about" ? "pointer-events-none cunderline":"")}>About</a>
-                    <a href="/projects" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
-                    <a href="/contact" className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
+                    <Link href="/">
+                        <a className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "home" ? "pointer-events-none underline":"")}>Home</a>
+                    </Link>
+                    <Link href="/about">
+                        <a className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "about" ? "pointer-events-none cunderline":"")}>About</a>
+                    </Link>
+                    <Link href="/projects">
+                        <a className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "projects" ? "pointer-events-none underline":"")}>Projects</a>
+                    </Link>
+                    <Link href="/contact">
+                        <a className={"w-full py-1 bg-slate-50 border-y border-slate-100 text-lg font-semibold text-center " + (page == "contact" ? "pointer-events-none underline":"")}>Contact</a>
+                    </Link>
                     <div className="h-screen" />
                 </div>
             </div> 
