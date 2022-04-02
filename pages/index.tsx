a9e1b85afc670441fc45352baad868ba3d1b4aa8
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-02 16:50:55 +0200

    Now uses next.js Link tag instead of regular anchor tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Container from '../components/Container'
 import Button from '../components/Button'
 import ProjectFrame from '../components/ProjectFrame'
 import Tag from '../components/Tag'
+import Link from 'next/link'
 import { getProjectsFromTitle } from '../lib/mongodb'
 import { FaGithub, FaLinkedin, FaDatabase } from 'react-icons/fa'
 import { SiCsharp, SiJava, SiJavascript, SiPython, SiTypescript, SiC, SiNextdotjs, SiReact, SiDocker, SiGit, SiNodedotjs, SiUnity, SiMongodb } from 'react-icons/si'
@@ -29,7 +30,7 @@ export default function Home({ projects }) {
                         </a>
                     </div>
                     <div className="mt-24">
-                        <h3 className="text-2xl font-medium text-center lg:text-left"><a href="/about" className="hover:text-cyan-500 duration-100">About me</a></h3>
+                        <h3 className="text-2xl font-medium text-center lg:text-left"><Link href="/about"><a className="hover:text-cyan-500 duration-100">About me</a></Link></h3>
                         <p className="text-lg text-center lg:text-left">
                             I've been programming for 7 years, starting out by making games and have ever since been intrigued by the many possibilities. 
                             I love exploring and learning new technologies, furthering my knowledge and improving my skills in the field. 
@@ -57,7 +58,7 @@ export default function Home({ projects }) {
                         </div>
                     </div>
                     <div className="mt-24">
-                        <h3 className="text-2xl font-medium text-center lg:text-left"><a href="/projects" className="hover:text-cyan-500 duration-100">My projects</a></h3>
+                        <h3 className="text-2xl font-medium text-center lg:text-left"><Link href="/projects"><a className="hover:text-cyan-500 duration-100">My projects</a></Link></h3>
                         <p className="text-lg text-center lg:text-left">
                             I enjoy exploring and learning, one of the best ways for me to get to know a new technology is to create something with that technology.
                             Below are a couple of the projects I've made, if you would like to see more of my projects you can do 
@@ -70,7 +71,7 @@ export default function Home({ projects }) {
                         </div>                     
                     </div>
                     <div className="mt-16 mb-8">
-                        <h3 className="text-2xl font-medium text-center lg:text-left"><a href="/contact" className="hover:text-cyan-500 duration-100">Get in touch</a></h3>
+                        <h3 className="text-2xl font-medium text-center lg:text-left"><Link href="/contact"><a className="hover:text-cyan-500 duration-100">Get in touch</a></Link></h3>
                         <p className="text-lg text-center lg:text-left">
                             If you would like to get in touch, you can do so by contacting me by email, LinkedIn or by filling in the contact form, 
                             all of which can be found <a href="/contact" className="underline font-medium hover:text-cyan-500 duration-100">here</a>. 
