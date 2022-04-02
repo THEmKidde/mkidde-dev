2dbcf189e409915fd9a69e81003c61a094a6b34e
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-02 17:24:46 +0200

    Fixed styling options

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,11 +1,11 @@
 
-export default function InputField({ type, name, id, placeholder="", label, required=false }) {
+export default function InputField({ type, name, id, placeholder="", label, required=false, className="" }) {
     return (
-        <div className={"m-3 " + (type == "textarea" ? "" : "w-1/2")}>
+        <div className={"m-3 " + className}>
             <label className="flex" htmlFor={id}>{label} {required ? <p className="text-red-600 ml-1">*</p> : ""}</label> 
             {type == "textarea" ? 
                 <textarea name={name} id={id} placeholder={placeholder} className="w-full h-32 px-2 py-1 rounded-md border" required={required} />
-                : <input type={type} name={name} id={id} placeholder={placeholder} className="px-2 py-1 rounded-md w-full border" required={required} />
+                : <input type={type} name={name} id={id} placeholder={placeholder} className="w-full px-2 py-1 rounded-md  border" required={required} />
             }
         </div>
     )
