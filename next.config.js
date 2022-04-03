ff8a2825cd11217eda2df74af0d9f890ac815f37
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-03 02:26:17 +0200

    added new picture domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
-      domains: ['media-exp1.licdn.com', 'www.adobe.com', 'i.ytimg.com'],
+      domains: ['i.imgur.com', 'media-exp1.licdn.com', 'www.adobe.com', 'i.ytimg.com'],
     },
     env: {
         FORM_URL: process.env.FORM_URL,
