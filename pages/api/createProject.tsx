669602be853a71898547856827d4a0d60eef5c95
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-15 17:55:03 +0200

    added itchio url to api

--- pages/api/createProject.tsx
+++ pages/api/createProject.tsx
@@ -5,7 +5,7 @@ const AUTH_TOKEN = process.env.AUTH_TOKEN;
 export default async function createProject(req, res) {
     if (req.body.token == AUTH_TOKEN) {
         try {
-            let project = {title:req.body.title, description:req.body.description, thumbnail_url:req.body.thumbnail_url, github_url:req.body.github_url}
+            let project = {title:req.body.title, description:req.body.description, thumbnail_url:req.body.thumbnail_url, github_url:req.body.github_url, itchio_url:req.body.itchio_url}
             await createNewProject(project).then(db_answer=>{
                 if(db_answer.acknowledged){
                     res.status(200).end();
