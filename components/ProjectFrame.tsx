6af76a9c2fa1a82c1007db8818d4752842bfde42
Mikkel Kidde Thomsen <45518395+THEmKidde@users.noreply.github.com>
2022-04-15 17:53:53 +0200

    added itchio icon if project has url for it

--- components/ProjectFrame.tsx
+++ components/ProjectFrame.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub, FaItchIo } from 'react-icons/fa'
 
 export default function ProjectFrame({ prop }) {
     return (
@@ -17,10 +17,21 @@ export default function ProjectFrame({ prop }) {
                 <div className="max-w-full h-36 mb-2 pr-2 overflow-hidden hover:overflow-y-scroll hover:pr-0">
                     <p className="text-base whitespace-pre-wrap stable-scrollbar">{prop.description}</p>
                 </div>
-                <div className="absolute top-0 z-100 mt-2 bg-slate-100 p-1 rounded-full">
-                    <a href={prop.github_url} target="_blank" className="text-5xl text-neutral-700 hover:text-cyan-500 duration-100" >
-                        <FaGithub />
-                    </a>
+                <div className="absolute top-0 z-100 mt-2 flex space-x-2">
+                    <div className="bg-slate-100 p-1 rounded-full">
+                        <a href={prop.github_url} target="_blank" className="text-5xl text-neutral-700 hover:text-cyan-500 duration-100" >
+                            <FaGithub />
+                        </a>
+                    </div>
+                    {prop.itchio_url == "" ? 
+                        <></>
+                        :
+                        <div className="bg-slate-100 p-1 rounded-2xl">
+                            <a href={prop.itchio_url} target="_blank" className="text-5xl text-neutral-700 hover:text-cyan-500 duration-100" >
+                                <FaItchIo />
+                            </a>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
