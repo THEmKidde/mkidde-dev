068772c7311bbf3e8006841fec6bafc53da346a9
Mikkel Kidde Thomsen <mkidde@outlook.dk>
2023-04-18 00:04:20 +0200

    Made the project frames more responsive and added text fade on bottom of description.

--- components/ProjectFrame.tsx
+++ components/ProjectFrame.tsx
@@ -3,7 +3,7 @@ import { FaGithub, FaItchIo } from 'react-icons/fa'
 
 export default function ProjectFrame({ prop }) {
     return (
-        <div className="mx-auto rounded-md bg-white border shadow-md max-w-xl relative mb-6 md:mb-12">
+        <div className="mx-auto rounded-md bg-white border shadow-md max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-md xl:max-w-xl relative mb-6 md:mb-12">
             <div className="rounded-t-md overflow-hidden">
                 <Image 
                     src={prop.thumbnail_url}
@@ -12,10 +12,10 @@ export default function ProjectFrame({ prop }) {
                     height={331}
                 />
             </div>
-            <div className="mx-2 my-1 h-48">
-                <h1 className="text-2xl max-w-lg">{prop.title}</h1>
-                <div className="max-w-full h-36 mb-2 pr-2 overflow-hidden hover:overflow-y-scroll hover:pr-0">
-                    <p className="text-base whitespace-pre-wrap stable-scrollbar">{prop.description}</p>
+            <div className="ml-2 mr-1 mt-1 mb-0 h-48 inner-shadow ">
+                <h1 className="text-2xl h-8 truncate pr-3">{prop.title}</h1>
+                <div className="h-40 mb-2 pr-2 overflow-hidden overflow-y-scroll">
+                    <p className="text-base whitespace-pre-wrap stable-scrollbar">{prop.description}</p><br></br>
                 </div>
                 <div className="absolute top-0 z-100 mt-2 flex space-x-2">
                     <div className="bg-slate-100 p-1 rounded-full">
